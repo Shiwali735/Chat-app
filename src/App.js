9bda2d0085266f28c5b201b1eedc44039df81643
Shiwali735 <shiwalibarnwal@gmail.com>
2023-01-31 15:58:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+ import ChatFeed from './components/ChatFeed';
+ import LoginForm from './components/LoginForm';
+import './App.css';
+
+// const projectID = 'e265cc9e-a9e8-4c4b-b1bf-a491a8e06f94';
+
+const App = () => {
+   if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="e265cc9e-a9e8-4c4b-b1bf-a491a8e06f94"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    //   onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
